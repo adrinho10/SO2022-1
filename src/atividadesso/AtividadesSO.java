2e2f2bcdf49aa9a1a52332b75b007897cc5f7fb3
ADRIAN PRATES TORRES <adriantorres9610@gmail.com>
2022-05-09 09:42:01 -0300

    primeiro commit

---
+++ src/atividadesso/AtividadesSO.java
@@ -0,0 +1,16 @@
+package atividadesso;
+import java.util.Scanner;
+public class AtividadesSO {
+
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    
+    int id;
+    int tempo_execucao;
+    int tempo_espera;
+    int n_processos;
+    
+     
+    }
+    
+}
